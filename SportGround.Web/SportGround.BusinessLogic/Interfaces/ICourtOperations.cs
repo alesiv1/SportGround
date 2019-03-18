3ab69944aedbaa906a48af382cde52f6a3e8ed90
alesiv <aner98lam@gmail.com>
2019-03-18 12:24:05 +0200

    Add business login in project and test data

---
+++ SportGround.Web/SportGround.BusinessLogic/Interfaces/ICourtOperations.cs
@@ -0,0 +1,15 @@
+﻿using SportGround.BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SportGround.BusinessLogic.Interfaces
+{
+	public interface ICourtOperations
+	{
+		CourtModel GetCourtById(int id);
+		List<CourtModel> GetAll();
+		void Create(CourtModel model);
+		void Update(int id, CourtModel model);
+		void Delete(int id);
+	}
+}
---
+++ SportGround.Web/SportGround.BusinessLogic/Operations/CourtOperations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SportGround.BusinessLogic.Interfaces;
+using SportGround.BusinessLogic.Models;
+
+namespace SportGround.BusinessLogic.Operations
+{
+	public class CourtOperations : ICourtOperations
+	{
+		private List<CourtModel> data = new List<CourtModel>();
+
+		public void Create(CourtModel model)
+		{
+			data.Add(model);
+		}
+
+		public void Delete(int id)
+		{
+			data.Remove(data.FirstOrDefault(s => s.Id == id));
+		}
+
+		public List<CourtModel> GetAll()
+		{
+			return data;
+		}
+
+		public CourtModel GetCourtById(int id)
+		{
+			return data.FirstOrDefault(s => s.Id == id);
+		}
+
+		public void Update(int id, CourtModel model)
+		{
+			var record = data.FirstOrDefault(s => s.Id == id);
+			if (record != null)
+			{
+				record.Id = model.Id;
+				record.Name = model.Name;
+			}
+		}
+	}
+}

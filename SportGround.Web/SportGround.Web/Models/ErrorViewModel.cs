e7b154f297b22189d434bd059037729b17d7f6d2
alesiv <aner98lam@gmail.com>
2019-03-18 12:09:59 +0200

    Create web project

---
+++ SportGround.Web/SportGround.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SportGround.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}

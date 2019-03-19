ed1b237c5d97d3360b6834ebb7e48ac32017e40c
alesiv <aner98lam@gmail.com>
2019-03-19 19:28:11 +0200

    Finish do logic for site

--- SportGround.Web/SportGround.Data/Context/DataContext.cs
+++ SportGround.Web/SportGround.Data/Context/DataContext.cs
@@ -18,18 +18,18 @@ namespace SportGround.Data.Context
 			return base.Set<TEntity>();
 		}
 
-		//protected override void OnModelCreating(DbModelBuilder modelBuilder)
-		//{
-		//	var typesToRegister = Assembly.GetExecutingAssembly().GetTypes()
-		//		.Where(type => !String.IsNullOrEmpty(type.Namespace))
-		//		.Where(type => type.BaseType != null && type.BaseType.IsGenericType
-		//		                                     && type.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>));
-		//	foreach (var type in typesToRegister)
-		//	{
-		//		dynamic configurationInstance = Activator.CreateInstance(type);
-		//		modelBuilder.Configurations.Add(configurationInstance);
-		//	}
-		//	base.OnModelCreating(modelBuilder);
-		//}
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			var typesToRegister = Assembly.GetExecutingAssembly().GetTypes()
+				.Where(type => !String.IsNullOrEmpty(type.Namespace))
+				.Where(type => type.BaseType != null && type.BaseType.IsGenericType
+													 && type.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>));
+			foreach (var type in typesToRegister)
+			{
+				dynamic configurationInstance = Activator.CreateInstance(type);
+				modelBuilder.Configurations.Add(configurationInstance);
+			}
+			base.OnModelCreating(modelBuilder);
+		}
 	}
 }
